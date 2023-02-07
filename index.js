84de7fd31ae90bf1da49d004edb9a7b2a1212168
Danushan Ravendran <it20051648@my.sliit.lk>
2023-02-07 17:01:16 +0530

    add index

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const MONGO_DB_CONFIG ={
-    DB : "mongodb://localhost/ecommerce-app"
-};
+const { error } = require("console");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const { MONGO_DB_CONFIG } = require("./config/app.config");
+const errors = require("./middleware/errors");
 
-module.exports ={
-    MONGO_DB_CONFIG
-}
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(MONGO_DB_CONFIG, {
+    useNewUrlPrser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("Database Connected");
+    },
+    (error) => {
+      console.log("not connected " + error);
+    }
+  );
+  
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+app.use("/api", require("./routes/app.routs"));
+app.use(error.errorHandler);
+
+app.listen(process.env.port || 4001, function () {
+  console.log("redy to go da fucker");
+});
