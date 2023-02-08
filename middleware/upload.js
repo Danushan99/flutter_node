76dfe69121d1652bcf1f17a0b7e586c8c11bce5c
Danushan Ravendran <it20051648@my.sliit.lk>
2023-02-08 10:20:46 +0530

    image upload

---
+++ middleware/upload.js
@@ -0,0 +1,26 @@
+const multer = require("multer");
+const { parse } = require("path");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destiation: function (req,file ,cb){
+        cb(null,"./uploads");
+
+    },
+    filename: function(req,file,cb){
+        cb(null,Date.now() + file.originalname);
+    }
+});
+
+const fileFilter =(req,file,callback ) =>{
+    const validExts = [".png",".jpg",".jpeg"];
+    if (!validExts.includes(path.extname(file.originalname))){
+        return callback (new Error ("only .png,.jpg,.jpeg formated allowd"));
+    }
+
+ const fileSize = parse(req.headers["content-length"]);
+ if (fileSize > 1048576){
+    return callback (new Error("file size to big"));
+ }
+ callback(null,true);
+}
